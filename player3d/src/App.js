3a8729046ed6e1876017e0736a057a4c1bfe202e
Nataliya Shipelova <n.shipelova@butik.ru>
2018-10-18 16:09:32 +0300

    feat (3D player)
    fetch data with axios from api

--- player3d/src/App.js
+++ player3d/src/App.js
@@ -1,25 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://private-5adf60-images3dapi.apiary-mock.com/images')
+      .then(response => {
+        // handle success
+        this.setState({images: response.data})
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+      })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+
       </div>
     );
   }
