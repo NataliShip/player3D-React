f09d5a0a4f4dd448a184d3adbf1a116ccea595b2
Nataliya Shipelova <n.shipelova@butik.ru>
2018-10-22 13:42:28 +0300

    feat (3D player)
    add styles

--- player3d/src/App.js
+++ player3d/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 import Player3D from './player-3d'
 
@@ -8,7 +8,9 @@ class App extends Component {
     super();
     this.state = {
       images: [],
+      playerActive: false,
     }
+    this.playerStart = this.playerStart.bind(this)
   }
 
   componentDidMount() {
@@ -23,21 +25,39 @@ class App extends Component {
       })
   }
 
+  playerStart() {
+    this.setState({playerActive: true})
+  }
+
   render() {
+    const {state: {playerActive, images}} = this;
+    const previewCss = playerActive ? 'hide' : ''
+    const playerCss = !playerActive ? 'hide' : ''
     return (
-      <div className='col-center'>
+      <div className='player3d'>
         <div>
-          <h2>3D обзор товара, через покадровую смену фотографий</h2>
-          <button id='start'>Старт</button>
-          <div className='container'>
+          <h2 className='player3d__header'>3D обзор товара, через покадровую смену фотографий</h2>
+          <p className='player3d__description'>Клик на превью для старта. Повторный клик остановит просмотр. Можно поворачивать с помощью мыши</p>
+          <div className='player3d__columns'>
+            <div className={`${playerCss} player3d__container`}>
+              {this.state.images.length > 0
+                ? < Player3D
+                  framesList={this.state.images}
+                  selectorStart='start'
+                  intervalDefault={200}
+                />
+                : null
+              }
+            </div>
+
             {this.state.images.length > 0
-              ? <Player3D
-                framesList={this.state.images}
-                selectorStart='start'
-                intervalDefault={200}
-              />
+              ? <div onClick={this.playerStart} id='start' className={`${previewCss} preview`}>
+                <img className='preview__image' src={images[0]} alt='preview'></img>
+                <span className='preview__play-icon'/>
+              </div>
               : null
             }
+
           </div>
         </div>
       </div>
